2d8e699ed66e606d1532abfbfc15ff0b8249cd2c
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-08-20 19:40:50 +0530

    Create SecondToHours.java

---
+++ basics/SecondToHours.java
@@ -0,0 +1,12 @@
+class SecondToHours{
+public static void main (String a[]){
+ 	long second=3600;
+	long min=second/60;
+	long hours=(second/60)/60;
+	System.out.println(min);
+	System.out.println(hours);
+
+
+}
+
+}
