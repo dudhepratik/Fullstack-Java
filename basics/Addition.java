d4a5b0d84dbe5c47efbcf5414277f25e848baeb5
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-08-20 09:16:08 +0530

    Create Addition.java

---
+++ basics/Addition.java
@@ -0,0 +1,19 @@
+class Addition{
+
+	public static void main(String args[]){
+			
+			int num1=11;
+			int num2=22;
+
+			int res=num1+num2;
+
+			System.out.println(res);
+
+			System.out.print("Addition is  ");
+			System.out.println(res);
+
+
+
+	}
+
+}
