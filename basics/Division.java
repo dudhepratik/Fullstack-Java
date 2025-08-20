709303cc57240dbc62c9b93aec0288c24513d7b3
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-08-20 19:28:14 +0530

    Create Division.java

---
+++ basics/Division.java
@@ -0,0 +1,13 @@
+class Division{
+
+	public static void main(String args[]){
+    
+			float num1=5.0;
+			int num2=2;
+
+			float res=num1/num2;
+			System.out.print("Division is  ");
+			System.out.println(res);
+	}
+
+}
