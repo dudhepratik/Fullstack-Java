aac7d290d567f6abcf4e04c41961702b82e9d66e
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-08-21 11:10:01 +0530

    Create CircleOperation.java

---
+++ basics/CircleOperation.java
@@ -0,0 +1,26 @@
+class CircleOperation{
+
+
+		public static void main(String a[]){
+
+            double radius=6.3;
+
+            double diameter=radius*2;
+
+            double circumference=2*3.14*radius;
+
+            double area=3.14*radius*radius;
+            System.out.print("diameter is  ");
+            System.out.println(diameter);
+
+            System.out.print("circumference is  ");
+            System.out.println(circumference);
+
+            System.out.print("area is  ");
+            System.out.println(area);
+
+
+		}
+
+
+}
