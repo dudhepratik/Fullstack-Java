f86632ae7dec1e40e9338c97280d9990168c8d65
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-09-04 21:47:52 +0530

    Create SquareNO.java

---
+++ Pattern/SquareNO.java
@@ -0,0 +1,22 @@
+/*
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+*/
+
+class Square{
+
+	public static void main(String[] args) {
+			int n=5;
+			for(int i=1;i<=n;i++){
+				for(int j=1;j<=n;j++){
+					System.out.print(j);
+				}
+
+				System.out.println();
+
+			}
+	}
