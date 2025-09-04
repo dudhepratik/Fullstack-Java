14136b4157d92bd2a7e537e055c9f37af107c7f0
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-09-04 21:58:27 +0530

    Create Square2.java

---
+++ Pattern/Square2.java
@@ -0,0 +1,22 @@
+/*
+55555
+44444
+33333
+22222
+11111
+*/
+class Square{
+
+	public static void main(String[] args) {
+			int n=5;
+			for(int i=n;i>=1;i--){
+				for(int j=1;j<=5;j++){
+					System.out.print(i +" ");
+				}
+
+				System.out.println();
+
+			}
+	}
+
+}
