ae207e09f1fbb971886bf7ee3259c7c938953c28
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-09-08 19:54:09 +0530

    Create Triangle2.java

---
+++ Pattern/Triangle2.java
@@ -0,0 +1,17 @@
+1
+22
+333
+4444
+55555
+*/
+class Program1{
+	public static void main(String[] args) {
+		int n=10;
+		for(int i=1;i<=n;i++){  // i++ ++i i+=1 i=i+1
+			for(int j=1;j<=i;j++){
+				 System.out.print(i+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
