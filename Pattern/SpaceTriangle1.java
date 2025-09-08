3877696fc8d36e810e220387485941d486aeedce
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-09-08 20:07:40 +0530

    Create SpaceTriangle1.java

---
+++ Pattern/SpaceTriangle1.java
@@ -0,0 +1,21 @@
+    *
+   **
+  ***
+ ****
+*****
+*/
+class Program{
+	public static void main(String[] args) {
+		int n=20;
+		for(int i=1;i<=n;i++){
+
+			for(int sp=i ; sp<n  ; sp++)	
+				System.out.print(" ");
+
+			for(int j=1;j<=i;j++)
+				System.out.print("*");
+
+			System.out.println();
+		}	
+	}
+}
