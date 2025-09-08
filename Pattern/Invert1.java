12a885d1e82aede5a9afc14b831ee7c57d2cb4fc
Pratik  Sanjayrao Dudhe <139216954+dudhepratik@users.noreply.github.com>
2025-09-08 20:17:47 +0530

    Create Invert1.java

---
+++ Pattern/Invert1.java
@@ -0,0 +1,25 @@
+55555
+ 4444
+  333
+   22
+    1
+
+
+
+class Program2{
+	public static void main(String[] args) {
+		
+		int n=5;
+		for(int i=n;i>=1;i--){
+
+			for(int sp=i;sp<n;sp++)
+				System.out.print(" ");			
+
+			for(int j=1;j<=i;j++)
+				System.out.print(i);
+			System.out.println();
+		
+		}
+
+	}
+}
